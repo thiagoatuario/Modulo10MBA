79fc00b0d4d3248d518074da87cca13e238a0782
thiagoatuario <53378967+thiagoatuario@users.noreply.github.com>
2020-10-24 17:54:45 -0300

    Update exercicio02.py
    Correção no código

--- exercicio02.py
+++ exercicio02.py
@@ -1,7 +1,10 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import TimeoutException, WebDriverException
+from selenium.webdriver.support.ui import WebDriverWait
 import json
+
 
 def finds(browser, by, expression):
     try:
@@ -14,32 +17,23 @@ def find(browser, by, expression):
         return browser.find_element(by, expression)
     except NoSuchElementException as nse:
         return None
-a = input("Qual item você procura? ")
+a=input("Selecione o item\n")
 output  = open('output.json', 'w', encoding='utf-8')
 driver = webdriver.Firefox()
-driver.get("https://lista.mercadolivre.com.br/" + a )
+driver.get("https://lista.mercadolivre.com.br/" + a)
 
-items = finds(driver, By.CLASS_NAME, 'ui-search-result__wrapper')
-while True:
-    for item in items:
-
-        descricao = find(item, By.CLASS_NAME, 'ui-search-item__title').get_property("textContent")
-        preco = find(item, By.CLASS_NAME, 'ui-search-price').get_property("textContent")
-        preco = preco.replace('R$', 'R$ ')
-            
-        print(descricao)
-    
-        item = {'descricao': descricao, 'preco': preco}
-    
-        print(preco)
-        output.write(json.dumps(item))
-        output.write('\n')
-    proxima = finds(driver, By.CLASS_NAME, 'ui-search-link')#'andes-pagination__link')
-    if proxima != None:
-        proxima.click()
-        print("Navegando para a proxima página")
-    else:
-        print("Ultima página carregada")
-        break
-
+items = finds(driver, By.CLASS_NAME, 'ui-search-result__content-wrapper')
+for item in items: 
+    descricao = find(item, By.CLASS_NAME, 'ui-search-item__title').get_property("textContent")
+    preco = find(item, By.CLASS_NAME, 'ui-search-price').get_property("textContent")
+        
+    print(descricao)
+        
+    print(preco)
+        
+    item = {'descricao': descricao, 'preco': preco}
+        
+    output.write(json.dumps(item))
+    output.write('\n')
+       
 driver.close()
